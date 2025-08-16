3482e1ad9df55d52d51c87410f7ce0e8d6e509c4
Sajitha94 <sajithasubramani94@gmail.com>
2025-08-16 12:30:37 +0530

    push

--- src/pages/productDetails.jsx
+++ src/pages/productDetails.jsx
@@ -37,7 +37,7 @@ function ProductDetails({ cart, setCart }) {
   }
 
   return (
-    <Box className=" p-5" sx={{pt:12}}>
+    <Box className=" p-5" sx={{ pt: 12 }}>
       {productMatch.map((item, idx) => (
         <Card
           className=" flex flex-col  justify-center items-center  p-2"
@@ -47,7 +47,14 @@ function ProductDetails({ cart, setCart }) {
             component="img"
             alt="green iguana"
             className="  rounded-lg "
-            sx={{ width: "50vw" }}
+            sx={{
+              width: "50vw",
+              objectFit: "cover",
+              height: {
+                xs: "25vh",
+                md: "100vh",
+              },
+            }}
             image={item.imageUrl || fallbackImage}
             onError={(e) => {
               e.target.src = fallbackImage;
@@ -56,8 +63,16 @@ function ProductDetails({ cart, setCart }) {
               navigate(`/product/${item.itemID}`);
             }}
           />
-          <CardContent className="flex flex-col gap-2 md:w-[50vw]  " style={{padding:"0" ,marginTop:"10px" ,paddingLeft:"5px",paddingRight:"5px"}}>
-            <Box className="flex justify-between items-center ">
+          <CardContent
+            className="flex flex-col gap-2 items-center md:w-[50vw]  "
+            style={{
+              padding: "8px",
+              marginTop: "10px",
+              paddingLeft: "5px",
+              paddingRight: "5px",
+            }}
+          >
+            <Box className="flex justify-around items-center gap-2   ">
               <Typography
                 level="title-lg"
                 sx={{ fontSize: "16px", fontWeight: 600, color: "#5d4037" }}
@@ -71,22 +86,49 @@ function ProductDetails({ cart, setCart }) {
                 ₹{item.itemPrice}
               </Typography>
             </Box>
-            <Box className="flex gap-2  justify-between ">
-              <Typography sx={{ fontSize: "13px", color: "gray",fontWeight:600 }}>
+            <Box
+              className="flex gap-2  justify-between items-center  "
+              
+            >
+              <Typography
+                sx={{
+                  fontSize: "13px",
+                  color: "gray",
+                  fontWeight: 600,
+                }}
+              >
                 {item.itemDescription}
               </Typography>
+              </Box>
+              <Box>
               <Typography
-                sx={{ fontSize: "14px", fontWeight: 500, color: "#795548",fontWeight:600  }}
+                sx={{
+                  fontSize: "14px",
+                  fontWeight: 500,
+                  color: "#795548",
+                  fontWeight: 600,
+                  width: "50vw",
+                   textAlign:"center"
+                }}
               >
                 {item.restaurantName}
               </Typography>
             </Box>
           </CardContent>
-          <CardActions className=" flex gap-2 justify-center" style={{padding:"3px", marginBottom:"20px"}}>
+          <CardActions
+            className=" flex gap-2 justify-center"
+            style={{ padding: "3px", marginBottom: "20px" }}
+          >
             {cart.some((cartItem) => cartItem.itemID == item.itemID) ? (
               <Box className="flex gap-2 items-center">
                 <Button
-                  sx={{ backgroundColor: "#6d4c41", minWidth: "10px",borderRadius:"50%",color:"white",paddingTop:"4px" }}
+                  sx={{
+                    backgroundColor: "#6d4c41",
+                    minWidth: "10px",
+                    borderRadius: "50%",
+                    color: "white",
+                    paddingTop: "4px",
+                  }}
                   onClick={() => {
                     setCart((prev) =>
                       prev.map((cartItem) =>
@@ -109,7 +151,13 @@ function ProductDetails({ cart, setCart }) {
                   }
                 </Typography>
                 <Button
-                  sx={{ backgroundColor: "#6d4c41", minWidth: "10px",borderRadius:"50%",color:"white",paddingTop:"4px" }}
+                  sx={{
+                    backgroundColor: "#6d4c41",
+                    minWidth: "10px",
+                    borderRadius: "50%",
+                    color: "white",
+                    paddingTop: "4px",
+                  }}
                   onClick={() => {
                     AddtoCart(item);
                   }}
